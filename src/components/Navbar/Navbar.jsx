3cf33b740ce0d153b889fc1fa7d6cd04529f02b2
G.Edison <g.edison.code@gmail.com>
2025-05-08 01:12:49 -0300

    Add Modal

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,47 @@
 import style from "./Navbar.module.css";
-import {useState} from "react"
+import { useState } from "react";
 
 function Navbar() {
-    const [mode, setMode] = useState(false)
+  const [mode, setMode] = useState(false);
+  const [modal, setModal] = useState(false);
 
-    function darkmode() {
-        setMode(!mode)
-        mode === false ? document.body.classList.add("darkMode") : document.body.classList.remove("darkMode")
-    }
+  function darkmode() {
+    setMode(!mode);
+    mode === false
+      ? document.body.classList.add("darkMode")
+      : document.body.classList.remove("darkMode");
+  }
+
+  function showModal() {
+    setModal(!modal);
+  }
 
   return (
     <>
       <nav className={style.mainContainer}>
-        <div ><h1 className={style.logo}>Logo</h1></div>
+        <div>
+          <h1 className={style.logo}>Logo</h1>
+        </div>
         <div className={style.buttons}>
-            <button onClick={darkmode} className={style.button}>Theme</button>
-            <button className={style.button}>About</button>
+          <button onClick={darkmode} className={style.button}>
+            Theme
+          </button>
+          <button className={style.button} onClick={showModal}>
+            About
+          </button>
         </div>
       </nav>
+
+      {modal && (
+        <div className={style.modalContainer}>
+            <div className={style.textModal}></div>
+          <h1>Contact: XXX.XXX.XXX</h1>
+          <h2>Email: XXXXX@XXX.com</h2>
+          <p>This is a random message</p>
+        </div>
+      )}
     </>
   );
 }
- 
 
-export default Navbar
+export default Navbar;
